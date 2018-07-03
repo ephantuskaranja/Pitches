21dc565b2a1cb2d801821759f81e0ea1f3f5c4db
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-03 10:17:22 +0300

    fix heroku deploy issues

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ephantus:switcher12@localhost/piches_app'
-
+    
 
 class ProdConfig(Config):
     '''
@@ -14,7 +13,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''    
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     '''
@@ -32,6 +31,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    DATABASE_URL = 'postgres://gabsykbpnmcoqo:2aefad3dea83969f994635b5722dcbed4765f137335adcc536c9d92249c22662@ec2-23-21-195-249.compute-1.amazonaws.com:5432/d3p4mmhs9n404v'
 
     DEBUG = True
 
